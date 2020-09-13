648898c8c4866aeedf4cd2b51a422b181495791b
Yong-Hae Lee <sundlee@gmail.com>
2020-09-13 23:54:34 +0900

    chapter 02 까지

---
+++ main.js
@@ -0,0 +1,44 @@
+(() => {
+  const stepElems = document.querySelectorAll('.step');
+  const graphicElems = document.querySelectorAll('.graphic-item');
+  let currentItem = graphicElems[0];
+  let ioIndex;
+
+  const io = new IntersectionObserver((entries, observer) => {
+    ioIndex = entries[0].target.dataset.index * 1;
+  });
+
+  for (let i = 0; i < stepElems.length; i++) {
+    io.observe(stepElems[i]);
+    stepElems[i].dataset.index = i;
+    graphicElems[i].dataset.index = i;
+  }
+
+  function activate() {
+    currentItem.classList.add('visible');
+  }
+
+  function inactivate() {
+    currentItem.classList.remove('visible');
+  }
+
+  window.addEventListener('scroll', () => {
+    let step;
+    let boundingRect;
+
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
+      step = stepElems[i];
+      if (!step) continue;
+      boundingRect = step.getBoundingClientRect();
+
+      if ((boundingRect.top > window.innerHeight * 0.1)
+      && (boundingRect.top < window.innerHeight *0.8)) {
+        inactivate();
+        currentItem = graphicElems[step.dataset.index];
+        activate();
+      }
+    }
+  });
+
+  activate();
+})();
