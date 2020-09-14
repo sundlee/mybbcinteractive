3d13ca4799c949d492b75e25855abe3384fd7752
Yong-Hae Lee <sundlee@gmail.com>
2020-09-14 10:05:52 +0900

    part 3까지 완료 (끝)

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 (() => {
+  const actions = {
+    birdFlies(key) {
+      if (key) {
+        document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+      } else {
+        document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+      }
+    },
+    birdFlies2(key) {
+      if (key) {
+        document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+      } else {
+        document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+      }
+    }
+  };
+
   const stepElems = document.querySelectorAll('.step');
   const graphicElems = document.querySelectorAll('.graphic-item');
   let currentItem = graphicElems[0];
@@ -14,12 +31,18 @@
     graphicElems[i].dataset.index = i;
   }
 
-  function activate() {
+  function activate(action) {
     currentItem.classList.add('visible');
+    if (action) {
+      actions[action](true);
+    }
   }
 
-  function inactivate() {
+  function inactivate(action) {
     currentItem.classList.remove('visible');
+    if (action) {
+      actions[action](false);
+    }
   }
 
   window.addEventListener('scroll', () => {
@@ -33,11 +56,15 @@
 
       if ((boundingRect.top > window.innerHeight * 0.1)
       && (boundingRect.top < window.innerHeight *0.8)) {
-        inactivate();
+        inactivate(currentItem.dataset.action);
         currentItem = graphicElems[step.dataset.index];
-        activate();
+        activate(currentItem.dataset.action);
       }
     }
+  });
+
+  window.addEventListener('load', () => {
+    setTimeout(() => scrollTo(0, 0), 100);
   });
 
   activate();
